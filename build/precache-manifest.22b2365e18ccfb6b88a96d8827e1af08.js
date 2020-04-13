329d72a4d18e57c4b454fa9cfb640df82e23f24b
verneri ver <verneri1@windowslive.com>
2020-04-13 13:43:26 +0300

    init

---
+++ build/precache-manifest.22b2365e18ccfb6b88a96d8827e1af08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1f7f8f812d3dd1f05cb0ddfa25e381",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3e1abf001819822f433",
+    "url": "/static/js/2.d1fa1e61.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d1fa1e61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41e1bbbcd7a6b06fb083",
+    "url": "/static/js/main.5e2a0de5.chunk.js"
+  },
+  {
+    "revision": "5cae70094d1da1591fe8",
+    "url": "/static/js/runtime-main.32c2e1c8.js"
+  }
+]);
